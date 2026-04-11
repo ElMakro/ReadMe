4672b41c3e8eb80dd0151dbe4fa764a53453cd6c
Alexander Evlampev <evlampev05@bk.ru>
2026-04-11 14:21:01 +0300

    Добавлено клиентское FastAPI приложение. Смонтирован путь под статический контент. index.html изменён для работы со статическим контентом. Добавлен Dockerfile для создания образа клиентской части. Изменён docker-compose.yml для добавления в оркестратор клиента.

---
+++ client/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi import Response
+from fastapi import status
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+
+client_app = FastAPI()
+
+client_app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+templates = Jinja2Templates(directory="frontend/templates")
+
+@client_app.get("/client_healthcheck")
+async def healthcheck():
+    return Response(status_code=status.HTTP_200_OK)
+
+
+@client_app.get("/")
+async def main_page(request: Request):
+    return templates.TemplateResponse(request, "index.html", {"request": request})
