0ced1b86a241ea206e7f8512396abb6a6dde2973
aniak <ania.klabukova@yandex.ru>
2026-04-26 12:31:09 +0300

    Добавлен маршрут регистрации и запуск приложения

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -1,1 +1,9 @@
 """Пакет для маршрутизации запросов в приложении"""
+from fastapi import APIRouter
+
+from server.app.api.auth_routes import auth_router
+
+
+app_router = APIRouter(prefix="/readme/v1")
+
+app_router.include_router(auth_router)
--- server/main.py
+++ server/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
-from fastapi import Response
-from fastapi import status
+import uvicorn
+from fastapi import FastAPI, Response, status
+
+from server.app.api import app_router
+from server.config.settings import settings
 
 app = FastAPI()
+app.include_router(app_router)
+
+
+def run():
+    uvicorn.run(app="main:app", port=settings.uvicorn_settings.server_port, reload=True)
+
 
 @app.get("/server_healthcheck")
 async def healthcheck():
