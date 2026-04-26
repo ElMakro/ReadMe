2b7a7f75b45ae632baa7bc62c2786e01ddfd4c32
aniak <ania.klabukova@yandex.ru>
2026-04-26 15:47:54 +0300

    Добавлены настройки Redis

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,18 @@ class DBSettings(BaseSettings):
         return f"postgresql+asyncpg://{self.db_user}:{self.db_password.get_secret_value()}@{self.db_host}:{self.db_port}/{self.db_name}"
 
 
+class RedisSettings(BaseSettings):
+    redis_host: str
+    redis_port: int
+    redis_db: int
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf8", extra="ignore")
+
+    @property
+    def redis_url(self):
+        return f"redis://{self.redis_host}:{self.redis_port}/{self.redis_db}"
+
+
 class UvicornSettings(BaseSettings):
     server_port: int
 
@@ -25,8 +37,10 @@ class UvicornSettings(BaseSettings):
 
 class Settings(BaseSettings):
     db_settings: DBSettings = DBSettings()
+    redis_settings: RedisSettings = RedisSettings()
     uvicorn_settings: UvicornSettings = UvicornSettings()
     secret_key: SecretStr
+    token_expire: int
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf8", extra="ignore")
 
