ac8b3e99cdef241548456198c936d8d2d747b391
aniak <ania.klabukova@yandex.ru>
2026-04-26 15:50:25 +0300

    Добавлена обработка аутентификации

--- server/app/service/users_manager.py
+++ server/app/service/users_manager.py
@@ -1,15 +1,20 @@
+import uuid
+
 from fastapi import Depends, HTTPException
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 from sqlalchemy.exc import IntegrityError
 
 from config.db_dependency import DBDependency
+from config.redis_dependency import RedisDependency
 from server.database.models import Users
-from server.schemas.users import NewUser, CreatedUserInfo
+from server.schemas.users import NewUser, CreatedUserInfo, UserInfo
 
 
 class UsersManager:
-    def __init__(self, db: DBDependency = Depends(DBDependency)) -> None:
+    def __init__(self, db: DBDependency = Depends(DBDependency), redis: RedisDependency = Depends(RedisDependency)) \
+            -> None:
         self.db = db
+        self.redis = redis
         self.model = Users
 
     async def create_user(self, user: NewUser) -> CreatedUserInfo:
@@ -25,3 +30,20 @@ class UsersManager:
 
             user_data = result.scalar_one()
             return CreatedUserInfo(**user_data.__dict__)
+
+    async def get_user_by_nickname(self, nickname: str) -> UserInfo | None:
+        async with self.db.db_session() as session:
+            query = select(
+                self.model.id,
+                self.model.nickname,
+                self.model.role,
+                self.model.password
+            ).where(self.model.nickname == nickname)
+
+            result = await session.execute(query)
+            user = result.mappings().first()
+            return UserInfo(**user) if user else None
+
+    async def store_token(self, token: str, user_id: uuid.UUID, session_id: str) -> None:
+        async with self.redis.get_client() as client:
+            await client.set(f"{user_id}:{session_id}", token)
--- server/app/service/users_service.py
+++ server/app/service/users_service.py
@@ -1,6 +1,10 @@
+from fastapi import HTTPException, status
+from fastapi.responses import JSONResponse
+
+from config.settings import settings
 from server.app.service.auth_handler import AuthHandler
 from server.app.service.users_manager import UsersManager
-from server.schemas.users import UserRegistration, CreatedUserInfo, NewUser
+from server.schemas.users import UserRegistration, CreatedUserInfo, NewUser, UserAuthentication
 
 
 class UsersService:
@@ -12,3 +16,24 @@ class UsersService:
         hashed_password = self.auth_handler.get_hashed_password(user.password)
         new_user = NewUser(email=user.email, nickname=user.nickname, password=hashed_password)
         return await self.manager.create_user(user=new_user)
+
+    async def login_user(self, user: UserAuthentication) -> JSONResponse:
+        existing_user = self.manager.get_user_by_nickname(user.nickname)
+        if existing_user is None or not await self.auth_handler.verify_password(
+                entered_password=user.password, hashed_password=existing_user.password
+        ):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Неверное имя пользователя или пароль."
+            )
+        token, session_id = await self.auth_handler.create_token(existing_user.id, existing_user.role)
+        await self.manager.store_token(token=token, user_id=existing_user.id, session_id=session_id)
+
+        response = JSONResponse(content={"message": "Успешный вход"})
+        response.set_cookie(
+            key="Authorization",
+            value=token,
+            httponly=True,
+            max_age=settings.token_expire
+        )
+        return response
