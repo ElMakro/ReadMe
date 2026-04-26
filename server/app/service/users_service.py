b1620a11d56a3d5819fb60a24f25e415ec40a6b4
aniak <ania.klabukova@yandex.ru>
2026-04-26 15:43:57 +0300

    Исправлена опечатка в названии поля

--- server/app/service/users_service.py
+++ server/app/service/users_service.py
@@ -6,9 +6,9 @@ from server.schemas.users import UserRegistration, CreatedUserInfo, NewUser
 class UsersService:
     def __init__(self, manager: UsersManager, auth_handler: AuthHandler) -> None:
         self.manager = manager
-        self.auth_handle = auth_handler
+        self.auth_handler = auth_handler
 
     async def register_user(self, user: UserRegistration) -> CreatedUserInfo:
-        hashed_password = self.auth_handle.get_hashed_password(user.password)
+        hashed_password = self.auth_handler.get_hashed_password(user.password)
         new_user = NewUser(email=user.email, nickname=user.nickname, password=hashed_password)
         return await self.manager.create_user(user=new_user)
