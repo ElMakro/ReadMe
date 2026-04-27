e772c543a93b595b62ee805193a898e0021d2c0c
aniak <ania.klabukova@yandex.ru>
2026-04-27 12:24:49 +0300

    Пакет настроек config перемещён в server

--- server/config/redis_dependency.py
+++ server/config/redis_dependency.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 
 from redis.asyncio import ConnectionPool, Redis
 
-from settings import settings
+from server.config.settings import settings
 
 
 class RedisDependency:
