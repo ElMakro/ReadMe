ee07d29a0d74edee3a0c7480a6331cc925eec08e
Alexander Evlampev <evlampev05@bk.ru>
2026-05-03 00:45:15 +0300

    Добавлен контейнер NoSQL БД Redis.

--- server/database/alembic/env.py
+++ server/database/alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from config.settings import settings
+from server.config.settings import settings
 from server.database.models import Base
 
 # this is the Alembic Config object, which provides
