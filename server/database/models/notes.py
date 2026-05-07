375e537bd1f1e3013952b0375945386153500126
aniak <ania.klabukova@yandex.ru>
2026-05-07 21:27:38 +0300

    Переименованы поля таблиц с id

--- server/database/models/notes.py
+++ server/database/models/notes.py
@@ -9,7 +9,7 @@ from server.database.models.base import Base
 
 
 class Notes(IDMixin, TimestampsMixin, Base):
-    student: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
-    topic: Mapped[uuid.UUID] = mapped_column(ForeignKey("topics.id", ondelete="CASCADE"))
+    student_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    topic_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("topics.id", ondelete="CASCADE"))
     name: Mapped[str] = mapped_column(String(255), nullable=True)
     content: Mapped[str] = mapped_column(Text, nullable=True)
