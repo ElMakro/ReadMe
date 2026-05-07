5bd0cc8b608b3af767643c7e51a2310e8d999cf1
Kirill Komogortcev <kirillkomogortcev@gmail.com>
2026-05-07 22:13:53 +0300

    Добавлено обращение к серверу при авторизации. Отключена защита от междоменных запросов для их корректной обработки к серверу при помощи Moscow Help

--- server/main.py
+++ server/main.py
@@ -1,10 +1,13 @@
 import uvicorn
 from fastapi import FastAPI, Response, status
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from server.app.api import app_router
 from server.config.settings import settings
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"])
 app.include_router(app_router)
 
 
