67309686c00f9ff5cdd0604b655adba2eb1ae0e8
aniak <ania.klabukova@yandex.ru>
2026-05-07 13:51:03 +0300

    Для таблиц sections и topics добавлено свойство ondelete cascade

--- server/database/models/sections.py
+++ server/database/models/sections.py
@@ -9,6 +9,6 @@ from server.database.models.base import Base
 
 
 class Sections(IDMixin, TimestampsMixin, Base):
-    course: Mapped[uuid.UUID] = mapped_column(ForeignKey("courses.id"))
+    course: Mapped[uuid.UUID] = mapped_column(ForeignKey("courses.id", ondelete="CASCADE"))
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     order_number: Mapped[int] = mapped_column(Integer, nullable=False)
