3450a22128d83e05b3790d5e3ab89f07e1ef2209
aniak <ania.klabukova@yandex.ru>
2026-05-13 19:17:30 +0300

    Добавлена пагинация для маршрута <мои курсы>

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -2,7 +2,9 @@
 from fastapi import APIRouter
 
 from server.app.api.auth_routes import auth_router
+from server.app.api.courses_routes import courses_router
 
 app_router = APIRouter(prefix="/readme/v1")
 
 app_router.include_router(auth_router)
+app_router.include_router(courses_router)
--- server/app/api/courses_routes.py
+++ server/app/api/courses_routes.py
@@ -1,10 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Query, status
 from fastapi.params import Depends
 
 from server.app.service.courses_service import CoursesService
-from server.app.service.depends import  get_current_user
+from server.app.service.depends import get_current_user
 from server.schemas.courses import CoursesList
 from server.schemas.users import UserVerification
 
@@ -17,5 +17,8 @@ courses_router = APIRouter(prefix="/courses", tags=["courses"])
     status_code=status.HTTP_200_OK
 )
 async def get_my_courses(user: Annotated[UserVerification, Depends(get_current_user)],
+                         page: int = Query(1, ge=1),
+                         size: int = Query(10, ge=1, le=20),
                          courses_service: CoursesService = Depends(CoursesService)) -> CoursesList:
-    return await courses_service.get_courses_for_user(user=user)
+    result = await courses_service.get_courses_for_user(user=user, page=page, size=size)
+    return result
