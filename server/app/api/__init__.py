99d293b84ce2d3cde033c68adc261d2d95de51bf
Alexander Evlampev <evlampev05@bk.ru>
2026-05-14 14:05:10 +0300

    Разрешена часть конфликтов слияния

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -1,10 +1,10 @@
 """Пакет для маршрутизации запросов в приложении"""
 from fastapi import APIRouter
 
-from server.app.api.students_routes import students_router
 from server.app.api.auth_routes import auth_router
 from server.app.api.courses_routes import courses_router
 from server.app.api.sections_routes import sections_router
+from server.app.api.students_routes import students_router
 from server.app.api.topics_router import topics_router
 
 app_router = APIRouter(
