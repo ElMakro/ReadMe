75917339e8a629a5a6006ef9a23b710f1fc9494a
Alexander Evlampev <evlampev05@bk.ru>
2026-05-15 12:25:21 +0300

    Добавлен запрос на создание курса.

--- server/app/service/courses_manager.py
+++ server/app/service/courses_manager.py
@@ -1,29 +1,110 @@
 import uuid
+from uuid import UUID
 
 from fastapi import Depends
-from sqlalchemy import select
+from sqlalchemy import select, insert
 
 from server.config.db_dependency import DBDependency
 from server.database.models import Courses, CoursesForStudents
-from server.schemas.courses import CoursesList
+from server.schemas.courses import CoursesList, CourseIDMixin, CourseResponse
+from server.schemas.users import UserVerification
 
 
 class CoursesManager:
-    def __init__(self, db: DBDependency = Depends(DBDependency)) -> None:
+    def __init__(
+            self,
+            db: DBDependency = Depends(
+                DBDependency,
+            ),
+    ) -> None:
         self.db = db
         self.courses_model = Courses
         self.courses_for_students_model = CoursesForStudents
 
-    async def get_courses_of_user(self, user_id: uuid.UUID, offset: int, limit: int) -> CoursesList:
+    async def get_courses_of_user(
+            self,
+            user_id: uuid.UUID,
+            offset: int,
+            limit: int,
+    ) -> CoursesList:
         async with self.db.db_session() as session:
             query = select(
-                self.courses_model.id, self.courses_model.name, self.courses_model.description
-                           ).join(
-                self.courses_for_students_model, self.courses_model.id == self.courses_for_students_model.course_id
-            ).where(self.courses_for_students_model.student_id == user_id
-                    ).order_by(self.courses_model.id
-                               ).offset(offset
-                                        ).limit(limit)
-            result = await session.execute(query)
+                self.courses_model.id,
+                self.courses_model.name,
+                self.courses_model.description,
+            ).join(
+                self.courses_for_students_model,
+                self.courses_model.id == self.courses_for_students_model.course_id,
+            ).where(
+                self.courses_for_students_model.student_id == user_id,
+            ).order_by(
+                self.courses_model.id,
+            ).offset(
+                offset,
+            ).limit(
+                limit,
+            )
+            result = await session.execute(
+                query,
+            )
             courses = result.mappings().all()
-            return CoursesList.model_validate(courses)
+            return CoursesList.model_validate(
+                courses,
+            )
+
+    async def create_course(
+            self,
+            name: str,
+            description: str,
+            professor_id: uuid.UUID,
+            is_open: bool,
+    ) -> CourseIDMixin:
+        async with self.db.db_session() as session:
+            query = insert(
+                self.courses_model,
+            ).values(
+                name=name,
+                description=description,
+                professor_id=professor_id,
+                is_open=is_open,
+            ).returning(
+                self.courses_model.id,
+            )
+            result = await session.execute(
+                query,
+            )
+            created_course = result.mappings().one()
+
+            await session.commit()
+            return CourseIDMixin.model_validate(
+                created_course,
+            )
+
+    async def get_course_by_id(
+            self,
+            user: UserVerification,
+            course_id: UUID,
+    ) -> CourseResponse | None:
+        async with self.db.db_session() as session:
+            query = select(
+                self.courses_model,
+            ).where(
+                self.courses_model.id == course_id,
+            )
+
+            result = await session.execute(
+                query,
+            )
+            course = result.mappings().one_or_none()
+            print(
+                course,
+            )
+
+        return CourseResponse.model_validate(
+            course,
+        ) if course else None
+
+    async def self_enroll_on_course(
+            self,
+    ):
+        pass
--- server/app/service/courses_service.py
+++ server/app/service/courses_service.py
@@ -1,8 +1,11 @@
-from fastapi import Depends
+from uuid import UUID
+
+from fastapi import Depends, HTTPException, status
 
 from server.app.service.auth_handler import AuthHandler
 from server.app.service.courses_manager import CoursesManager
-from server.schemas.courses import CoursesList
+from server.enums.role import Role
+from server.schemas.courses import CoursesList, CourseIDMixin, CourseResponse
 from server.schemas.users import UserVerification
 
 
@@ -19,7 +22,53 @@ class CoursesService:
         self.courses_manager = manager
         self.auth_handler = auth_handler
 
-    async def get_courses_for_user(self, user: UserVerification, page: int, size: int) -> CoursesList:
+    async def get_courses_for_user(
+            self,
+            user: UserVerification,
+            page: int,
+            size: int,
+    ) -> CoursesList:
         offset = (page - 1) * size
         limit = size
-        return await self.courses_manager.get_courses_of_user(user.id, offset, limit)
+        return await self.courses_manager.get_courses_of_user(
+            user.id,
+            offset,
+            limit,
+        )
+
+    async def create_course(
+            self,
+            user: UserVerification,
+            name: str,
+            description: str,
+            is_open: bool,
+    ) -> CourseIDMixin:
+        if user.role == Role.STUDENT:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+            )
+        return await self.courses_manager.create_course(
+            name=name,
+            description=description,
+            professor_id=user.id,
+            is_open=is_open,
+        )
+
+    async def get_course_by_id(
+            self,
+            user: UserVerification,
+            course_id: UUID,
+    ) -> CourseResponse:
+        course = await self.courses_manager.get_course_by_id(
+            user,
+            course_id,
+        )
+
+        if course:
+            return CourseResponse.model_validate(
+                course,
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+            )
