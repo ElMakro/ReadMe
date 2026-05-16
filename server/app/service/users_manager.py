f39749618d28a11e5811b92fc72a12ba4b7af059
aniak <ania.klabukova@yandex.ru>
2026-05-16 12:02:32 +0300

    Правки ruff

--- server/app/service/users_manager.py
+++ server/app/service/users_manager.py
@@ -7,7 +7,7 @@ from sqlalchemy.exc import IntegrityError
 from server.config.db_dependency import DBDependency
 from server.config.redis_dependency import RedisDependency
 from server.database.models import Users
-from server.schemas.users import CreatedUserInfo, NewUser, UserInfo, UserVerification, StoredUserInfo
+from server.schemas.users import CreatedUserInfo, NewUser, StoredUserInfo, UserInfo, UserVerification
 
 
 class UsersManager:
