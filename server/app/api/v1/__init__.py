3b51074bb0d16fd44aa3f0811b2802e3b9385060
aniak <ania.klabukova@yandex.ru>
2026-05-17 19:11:19 +0300

    Маршрутизатор для конспектов подключен к общему маршрутизатору

--- server/app/api/v1/__init__.py
+++ server/app/api/v1/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 
 from server.app.api.v1.auth.auth_routes import auth_router
 from server.app.api.v1.courses.courses_routes import courses_router
+from server.app.api.v1.notes import notes_router
 from server.app.api.v1.sections.sections_routes import sections_router
 from server.app.api.v1.topics.topics_router import topics_router
 from server.app.api.v1.users.users_routes import users_router
@@ -25,4 +26,7 @@ v1_router.include_router(
 )
 v1_router.include_router(
     users_router,
+)
+v1_router.include_router(
+    notes_router,
 )
