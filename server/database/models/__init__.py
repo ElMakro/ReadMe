cd63bb35af9c4465388662e39b74fe25ce11d032
aniak <ania.klabukova@yandex.ru>
2026-05-23 16:18:05 +0300

    Добавлена таблица заявок на роль преподавателя

--- server/database/models/__init__.py
+++ server/database/models/__init__.py
@@ -4,10 +4,11 @@ from server.database.models.base import Base
 from server.database.models.courses import Courses
 from server.database.models.courses_for_students import CoursesForStudents
 from server.database.models.notes import Notes
+from server.database.models.professors_applications import ProfessorsApplications
 from server.database.models.professors_details import ProfessorsDetails
 from server.database.models.sections import Sections
 from server.database.models.topics import Topics
 from server.database.models.users import Users
 
 __all__ = ("Base", "Users", "Courses", "CoursesForStudents", "Sections", "Topics", "Achievements",
-           "AchievementsForStudents", "Notes", "ProfessorsDetails")
+           "AchievementsForStudents", "Notes", "ProfessorsDetails", "ProfessorsApplications")
---
+++ server/database/models/professors_applications.py
@@ -0,0 +1,29 @@
+import uuid
+
+from sqlalchemy import Enum as PGEnum, Index
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from server.database.mixins.id_mixins import IDMixin
+from server.database.mixins.timestamp_mixins import TimestampsMixin
+from server.database.models.base import Base
+from server.enums.application_status import ApplicationStatus
+
+
+class ProfessorsApplications(IDMixin, TimestampsMixin, Base):
+    __tablename__ = "professors_applications"
+
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    name: Mapped[str] = mapped_column(String(32), nullable=False)
+    surname: Mapped[str] = mapped_column(String(32), nullable=False)
+    patronymic: Mapped[str] = mapped_column(String(32), nullable=True)
+    status: Mapped[ApplicationStatus] = mapped_column(
+        PGEnum(ApplicationStatus, native_enum=True),
+        unique=False,
+        nullable=False,
+        default=ApplicationStatus.PENDING,
+    )
+
+    __table_args__ = (
+        Index("idx_status_created", "status", "created_at"),
+    )
