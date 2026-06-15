31b4ebde3fa91d5f150420bd8f88cf62963c0143
Alexander Evlampev <evlampev05@bk.ru>
2026-06-15 11:23:12 +0300

    Убран тест-заглушка

--- server/tests/test_healthcheck.py
+++
@@ -1,9 +0,0 @@
-from fastapi import status
-
-from server.main import healthcheck
-
-
-async def test_server_healthcheck():
-    response = await healthcheck()
-
-    assert response.status_code == status.HTTP_200_OK
